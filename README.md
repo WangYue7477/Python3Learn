eaaaebf7401b31b88b7e07f5b0f66f966c6be3f4
WangYue <wangyue7477@gmail.com>
2017-09-26 00:09:51 +0800

    clean and start!

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Python3Learn
 This is my study workplace
-#Test pull!
+
+clean and start!
--- templateTest.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2017/9/25 16:32
-# @Author  : WangYue
-# @File    : templateTest.py
-# @Software: PyCharm Community Edition
-print("Hello world!")
