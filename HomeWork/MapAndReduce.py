0bafde509131d9bea237f7a78d9d3e96deb90193
John Wang <johnwang@nuskin.com>
2017-09-26 13:16:47 +0800

    homework2

--- HomeWork/MapAndReduce.py
+++ HomeWork/MapAndReduce.py
@@ -4,3 +4,10 @@
 # @Author  : WangYue
 # @File    : MapAndReduce.py
 # @Software: PyCharm Community Edition
+
+from functools import reduce
+
+def prod(L):
+    return reduce(lambda x,y:x*y,L)
+
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
