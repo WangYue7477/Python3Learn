fd414f70954fc657b0b1bb531b03d85653111b22
WangYue <wangyue7477@gmail.com>
2017-09-27 23:14:06 +0800

    change file name

--- Base/reducelearn.py
+++ Base/reducelearn.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2017/9/26 12:42
 # @Author  : WangYue
-# @File    : reducetest.py
+# @File    : reducelearn.py
 # @Software: PyCharm Community Edition
 
 from functools import reduce
