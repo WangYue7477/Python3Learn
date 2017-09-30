3e171aa3d62b18183024914409e89b26bd8438c2
WangYue <wangyue7477@gmail.com>
2017-09-30 22:30:51 +0800

    Filter Learn

--- Base/FilterLearn.py
+++ Base/FilterLearn.py
@@ -8,5 +8,21 @@ def is_odd(n):
     return n % 2 == 1
 print(list(filter(is_odd,[1,3,4,5,6,7,8,9,11])))
 
-s = ""
-print(s.strip())
+def _odd_iter():
+    n=1
+    while True:
+        n = n + 2
+        yield n
+def not_divisible(n):
+    return lambda x : x % n > 0
+def primes():
+    yield 2
+    it = _odd_iter()
+    while True:
+        n = next(it)
+        yield n
+        it = filter(not_divisible(n),it)
+for n in primes():
+    print(n)
+    if n > 1000:
+        break
