5386b3c12fa43211b9be0965f00dad51e2948c45
WangYue <wangyue7477@gmail.com>
2017-10-24 23:34:15 +0800

    Class and Instance begain

---
+++ ClassAndInstance/Student_ClassLearn.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/10/24 23:13
+# @Author  : WangYue
+# @File    : Student_ClassLearn.py
+# @Software: PyCharm
+
+class Student(object):
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def print_age(self):
+        print("%s:%s" % (self.name,self.age))
+
+    def get_age(self):
+        if self.age >= 30:
+            return "Old"
+        else:
+            return "Young"
+
+jim = Student('jim',23)
+
+print(jim.name)
+print(jim.age)
+jim.print_age()
+print(jim.get_age())
+
---
+++ ClassAndInstance/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/10/24 23:10
+# @Author  : WangYue
+# @File    : __init__.py.py
+# @Software: PyCharm
